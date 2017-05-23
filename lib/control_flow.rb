8c5f7e67d1dad540bdf02dea9fa3fe7c049325ae
SANPAK Ng <sanpakng@SANPAKs-Air.fios-router.home>
2017-05-22 21:39:45 -0400

    done

--- lib/control_flow.rb
+++ lib/control_flow.rb
@@ -38,7 +38,11 @@ end
 # of all whole numbers between 1 and the number itself. Assume the argument will
 # be > 0.
 def factorial(num)
-
+  result = 1
+  (2..num).each do |number|
+    result *= number
+  end
+  result
 end
 
 
@@ -47,6 +51,16 @@ end
 # Write your own version of the join method. separator = "" ensures that the
 # default seperator is an empty string.
 def my_join(arr, separator = "")
+  result = ""
+  (0...arr.length).each do |idx|
+    if idx != arr.length - 1
+      result << arr[idx] << separator
+    else
+      result << arr[idx]
+    end
+  end
+
+  return result
 
 end
 
@@ -54,14 +68,30 @@ end
 # character is lowercase and every even is uppercase, e.g.
 # weirdcase("weirdcase") => "wEiRdCaSe"
 def weirdcase(str)
-
+  (0...str.length).each do |idx|
+    if idx % 2 == 0
+      str[idx] = str[idx].downcase
+    else
+      str[idx] = str[idx].upcase
+    end
+  end
+  str
 end
 
 # Reverse all words of five more more letters in a string. Return the resulting
 # string, e.g., reverse_five("Looks like my luck has reversed") => "skooL like
 # my luck has desrever")
 def reverse_five(str)
-
+  result = []
+  str = str.split(" ")
+  str.each do |word|
+    if word.length > 4
+      result << word.reverse
+    else
+      result << word
+    end
+  end
+  result.join(" ")
 end
 
 # Return an array of integers from 1 to n (inclusive), except for each multiple
@@ -69,7 +99,19 @@ end
 # integer with "buzz", and for each multiple of both 3 and 5, replace the
 # integer with "fizzbuzz".
 def fizzbuzz(n)
-
+  array = []
+  (1..n).each do |num|
+    if num % 15 == 0
+      array << "fizzbuzz"
+    elsif num % 3 == 0
+      array << "fizz"
+    elsif num % 5 == 0
+      array << "buzz"
+    else
+      array << num
+    end
+  end
+  return array
 end
 
 
@@ -78,27 +120,41 @@ end
 # Write a method that returns a new array containing all the elements of the
 # original array in reverse order.
 def my_reverse(arr)
+  arr = arr.reverse
 
 end
 
 # Write a method that returns a boolean indicating whether the argument is
 # prime.
 def prime?(num)
-
+  return false if num == 1
+  (2...num).each do |number|
+    return false if num % number == 0
+  end
+  return true
 end
 
 # Write a method that returns a sorted array of the factors of its argument.
 def factors(num)
-
+  array = []
+  (1..num).each do |number|
+    array << number if num % number == 0
+  end
+  array.sort!
 end
 
 # Write a method that returns a sorted array of the prime factors of its argument.
 def prime_factors(num)
-
+  array = []
+  (1..num).each do |number|
+    array << number if prime?(number) && num % number == 0
+  end
+  array.sort!
 end
 
 # Write a method that returns the number of prime factors of its argument.
 def num_prime_factors(num)
+  prime_factors(num).length
 
 end
 
@@ -108,5 +164,9 @@ end
 # Return the one integer in an array that is even or odd while the rest are of
 # opposite parity, e.g. oddball([1,2,3]) => 2, oddball([2,4,5,6] => 5)
 def oddball(arr)
+  even_arr = arr.select { |n| n % 2 == 0 }
+  odd_arr = arr.reject { |n| n % 2 == 0 }
 
+  return even_arr[0] if even_arr.length == 1
+  return odd_arr[0]
 end
